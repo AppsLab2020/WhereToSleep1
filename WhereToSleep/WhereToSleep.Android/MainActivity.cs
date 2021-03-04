ac2c7f6d43c1be70886b357e9da1869929982f92
MartinKrause21 <martin.21.krause@gmail.com>
2021-03-04 20:46:46 +0100

    add splash screen

--- WhereToSleep/WhereToSleep.Android/MainActivity.cs
+++ WhereToSleep/WhereToSleep.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace WhereToSleep.Droid
 {
-    [Activity(Label = "WhereToSleep", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "WhereToSleep", Icon = "@drawable/logowts", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
