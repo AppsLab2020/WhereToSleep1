eb673a83b5b7807e15c2538f4c8af2a4fc5eb512
MartinKrause21 <martin.21.krause@gmail.com>
2021-04-08 13:10:35 +0200

    Fixed calendar bugs

--- WhereToSleep/WhereToSleep/MainPage.xaml.cs
+++ WhereToSleep/WhereToSleep/MainPage.xaml.cs
@@ -55,8 +55,7 @@ namespace WhereToSleep
 
         void Recalculate()
         {
-            TimeSpan timeSpan = endDatePicker.Date - startDatePicker.Date +
-                (includeSwitch.IsToggled ? TimeSpan.FromDays(1) : TimeSpan.Zero);
+            TimeSpan timeSpan = endDatePicker.Date - startDatePicker.Date;
 
             resultLabel.Text = String.Format("{0} day{1} between dates",
                                                 timeSpan.Days, timeSpan.Days == 1 ? "" : "s");
