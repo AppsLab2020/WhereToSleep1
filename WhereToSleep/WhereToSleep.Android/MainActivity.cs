f446c5bff8987e1f8d0671095175ef2ab948936d
simon.kvasnovsky@gmail.com <simon.kvasnovsky@gmail.com>
2021-04-14 22:13:57 +0200

    Add user location. Add display alert for using location.

--- WhereToSleep/WhereToSleep.Android/MainActivity.cs
+++ WhereToSleep/WhereToSleep.Android/MainActivity.cs
@@ -7,6 +7,10 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 
+using System.IO;
+using Plugin.Permissions;
+using Plugin.CurrentActivity;
+
 namespace WhereToSleep.Droid
 {
     [Activity(Label = "WhereToSleep", Icon = "@drawable/logowts", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
@@ -18,15 +22,19 @@ namespace WhereToSleep.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
-            
+
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            Plugin.CurrentActivity.CrossCurrentActivity.Current.Init(this, savedInstanceState);
             LoadApplication(new App());
             Xamarin.FormsMaps.Init(this, savedInstanceState);
+           
             
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
--- WhereToSleep/WhereToSleep/Views/MapPage.xaml.cs
+++ WhereToSleep/WhereToSleep/Views/MapPage.xaml.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,12 +18,40 @@ namespace WhereToSleep.Views
         public MapPage()
         {
             InitializeComponent();
+
             GetPermissons(); 
         }
         private async void GetPermissons()
         {
-            
+            try
+            {
+                var status = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.LocationWhenInUse);
+            if (status != PermissionStatus.Granted)
+            {
+       
+                if (await CrossPermissions.Current.ShouldShowRequestPermissionRationaleAsync(Permission.LocationWhenInUse))
+                {
+                    await DisplayAlert("We need your location", "We need to access your location", "OK");
+                }
+
+                var results = await CrossPermissions.Current.RequestPermissionsAsync(Permission.LocationWhenInUse);
+                if (results.ContainsKey(Permission.LocationWhenInUse))
+                status = results[Permission.LocationWhenInUse];
+            }
+            if(status == PermissionStatus.Granted)
+            {
+                LocationMap.IsShowingUser = true;
+            }
+                else
+                {
+                    await DisplayAlert("We need your location", "We need to access your location", "OK");
+                }
+           }
+            catch(Exception ex)
+            {
+                DisplayAlert("Error", ex.Message, "OK");
+            }
         }
-      
+        
     }
 }
